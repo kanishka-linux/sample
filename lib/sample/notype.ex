969a526a54d7fe2a63653a94e74131b9efa75e53
kanishka-linux <kanishka.linux@gmail.com>
2018-11-11 01:16:33 +0530

    add Notype module

---
+++ lib/sample/notype.ex
@@ -0,0 +1,34 @@
+defmodule SM.Notype do
+  @prop %{
+    "minLength" => "string",
+    "maxLength" => "string",
+    "pattern" => "string",
+    "multipleOf" => "number",
+    "minimum" => "number",
+    "maximum" => "number",
+    "exclusiveMinimum" => "number",
+    "exclusiveMaximum" => "number",
+    "items" => "array",
+    "additionalItems" => "array",
+    "minItems" => "array",
+    "maxItems" => "array",
+    "uniqueItems" => "array",
+    "properties" => "object",
+    "additionalProperties" => "object",
+    "required" => "object",
+    "minProperties" => "object",
+    "maxProperties" => "object"
+  }
+
+  def gen_notype(map, type) do
+    nmap = for {k, v} <- map, into: %{}, do: {k, v}
+    nlist = for {k, v} <- map, into: [], do: @prop[k]
+    types =
+      Enum.reduce(nlist, nil, fn
+        x, acc when not is_nil(x) -> x
+        x, acc when is_nil(x) -> acc
+      end)
+    nmap = if not is_nil(types), do: Map.put(nmap, "type", types), else: nmap
+    if nmap["type"], do: SM.gen_init(nmap)
+  end
+end
