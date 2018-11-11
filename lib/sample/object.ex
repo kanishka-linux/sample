7c87f017c9900055d2b0a790b813824cddf71c74
kanishka-linux <kanishka.linux@gmail.com>
2018-11-12 01:22:42 +0530

    define global min max properties

--- lib/sample/object.ex
+++ lib/sample/object.ex
@@ -8,14 +8,35 @@ defmodule SM.Object do
     nil
   ]
 
+  @min_properties 0
+
+  @max_properties 1000
+
+  def get_min_max(map) do
+    min =
+      if map["minProperties"] do
+        min = map["minProperties"]
+      else
+        min = @min_properties
+      end
+
+    max =
+      if map["maxProperties"] do
+        max = map["maxProperties"]
+      else
+        max = @max_properties
+      end
+
+    {min, max}
+  end
+
   def gen_object(map, type), do: objectype(map, map["enum"], map["properties"])
 
   def objectype(map, enum, properties) when enum != nil, do: SM.gen_enum(enum, "object")
 
   def objectype(map, enum, properties) when is_nil(properties) and is_nil(enum) do
-    list = for n <- @type_list, is_map(n), do: SM.gen_init(n)
-    key = SM.gen_init(%{"type" => "string", "maxLength" => 10, "minLength" => 4})
-    decide_min_max(map, key, StreamData.one_of(list), map["minProperties"], map["maxProperties"])
+    {min, max} = get_min_max(map)
+    decide_min_max(map, SM.gen_init(%{"type" => "string"}), StreamData.term(), min, max)
   end
 
   def objectype(map, enum, properties) when is_map(properties) do
@@ -40,55 +61,48 @@ defmodule SM.Object do
 
   def bind_function(new_prop, additional, y, z) do
     StreamData.bind(StreamData.optional_map(new_prop), fn mapn ->
-      StreamData.bind(
+      StreamData.bind_filter(
         additional,
         fn
           nmap
-          when (not is_nil(y) and map_size(mapn) + map_size(nmap) < y) or
-                 (not is_nil(z) and map_size(mapn) + map_size(nmap) > z) ->
-            StreamData.constant(%{})
+          when (map_size(mapn) + map_size(nmap)) in y..z ->
+            {:cont, StreamData.constant(Map.merge(mapn, nmap))}
+
+          nmap when map_size(mapn) in y..z ->
+            {:cont, StreamData.constant(mapn)}
 
           nmap when is_map(nmap) ->
-            StreamData.constant(Map.merge(mapn, nmap))
+            :skip
         end
       )
     end)
   end
 
   def bind_function_req(req, non_req, y, z) when is_map(non_req) or is_nil(non_req) do
-    StreamData.bind(
+    StreamData.bind_filter(
       StreamData.fixed_map(req),
       fn
         mapn when is_map(non_req) ->
-          StreamData.bind(StreamData.optional_map(non_req), fn
-            nmap
-            when not is_nil(y) and map_size(mapn) + map_size(nmap) >= y and
-                   (not is_nil(z) and map_size(mapn) + map_size(nmap) <= z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
+          {:cont,
+           StreamData.bind_filter(StreamData.optional_map(non_req), fn
+             nmap
+             when (map_size(mapn) + map_size(nmap)) in y..z ->
+               {:cont, StreamData.constant(Map.merge(mapn, nmap))}
 
-            nmap
-            when is_nil(y) and (not is_nil(z) and map_size(mapn) + map_size(nmap) <= z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
+             nmap
+             when map_size(mapn) in y..z ->
+               {:cont, StreamData.constant(mapn)}
 
-            nmap
-            when not is_nil(y) and map_size(mapn) + map_size(nmap) >= y and is_nil(z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
-
-            nmap
-            when is_nil(y) and is_nil(z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
-
-            nmap when true ->
-              StreamData.constant(%{})
-          end)
+             nmap when true ->
+               :skip
+           end)}
 
         mapn
-        when is_nil(non_req) and
-               ((not is_nil(y) and map_size(mapn) < y) or (not is_nil(z) and map_size(mapn) > z)) ->
-          StreamData.constant(%{})
+        when is_nil(non_req) and map_size(mapn) in y..z ->
+          {:cont, StreamData.constant(mapn)}
 
         mapn when is_nil(non_req) ->
-          StreamData.constant(mapn)
+          :skip
       end
     )
   end
@@ -98,26 +112,17 @@ defmodule SM.Object do
       StreamData.fixed_map(req),
       fn
         mapn when is_map(non_req) ->
-          StreamData.bind(non_req, fn
+          StreamData.bind_filter(non_req, fn
             nmap
-            when not is_nil(y) and map_size(mapn) + map_size(nmap) >= y and
-                   (not is_nil(z) and map_size(mapn) + map_size(nmap) <= z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
+            when (map_size(mapn) + map_size(nmap)) in y..z ->
+              {:cont, StreamData.constant(Map.merge(mapn, nmap))}
 
             nmap
-            when is_nil(y) and (not is_nil(z) and map_size(mapn) + map_size(nmap) <= z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
-
-            nmap
-            when not is_nil(y) and map_size(mapn) + map_size(nmap) >= y and is_nil(z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
-
-            nmap
-            when is_nil(y) and is_nil(z) ->
-              StreamData.constant(Map.merge(mapn, nmap))
+            when map_size(mapn) in y..z ->
+              {:cont, StreamData.constant(mapn)}
 
             nmap when true ->
-              StreamData.constant(%{})
+              :skip
           end)
       end
     )
@@ -125,33 +130,37 @@ defmodule SM.Object do
 
   def check_additional_properties(map, req_size, req, non_req, new_prop)
       when is_nil(req) or req_size == 0 do
-    case {map["additionalProperties"], map["minProperties"], map["maxProperties"]} do
+    {min, max} = get_min_max(map)
+
+    case {map["additionalProperties"], min, max} do
       {x, y, z} when is_nil(x) or (is_boolean(x) and x) ->
         additional = objectype(map, nil, nil)
         bind_function(new_prop, additional, y, z)
 
       {x, y, z} when is_boolean(x) and not x ->
-        StreamData.bind(
+        StreamData.bind_filter(
           StreamData.optional_map(new_prop),
           fn
             map
-            when (not is_nil(y) and map_size(map) < y) or (not is_nil(z) and map_size(map) > z) ->
-              StreamData.constant(%{})
+            when map_size(map) in y..z ->
+              {:cont, StreamData.constant(map)}
 
             map when true ->
-              StreamData.constant(map)
+              :skip
           end
         )
 
       {x, y, z} when is_map(x) ->
         obj = SM.gen_init(x)
-        key = SM.gen_init(%{"type" => "string", "maxLength" => 10, "minLength" => 4})
+        key = SM.gen_init(%{"type" => "string"})
         bind_function(new_prop, StreamData.map_of(key, obj), y, z)
     end
   end
 
   def check_additional_properties(map, req_size, req, non_req, new_prop) when req_size > 0 do
-    case {map["additionalProperties"], map["minProperties"], map["maxProperties"]} do
+    {min, max} = get_min_max(map)
+
+    case {map["additionalProperties"], min, max} do
       {x, y, z} when is_nil(x) or (is_boolean(x) and x) ->
         additional = objectype(map, nil, nil)
         val2 = bind_function(non_req, additional, y, z)
@@ -162,7 +171,7 @@ defmodule SM.Object do
 
       {x, y, z} when is_map(x) ->
         obj = SM.gen_init(x)
-        key = SM.gen_init(%{"type" => "string", "maxLength" => 10, "minLength" => 4})
+        key = SM.gen_init(%{"type" => "string"})
         val1 = decide_min_max(map, key, obj, y, z)
         val2 = bind_function(non_req, val1, y, z)
         bind_function_req(req, val2, y, z, "additional")
@@ -170,19 +179,7 @@ defmodule SM.Object do
   end
 
   def decide_min_max(map, key, value, min, max)
-      when is_integer(min) and is_integer(max) and min < max do
+      when is_integer(min) and is_integer(max) and min <= max do
     StreamData.map_of(key, value, min_length: min, max_length: max)
-  end
-
-  def decide_min_max(map, key, value, min, max) when is_integer(min) and is_nil(max) do
-    StreamData.map_of(key, value, min_length: min)
-  end
-
-  def decide_min_max(map, key, value, min, max) when is_nil(min) and is_integer(max) do
-    StreamData.map_of(key, value, max_length: max)
-  end
-
-  def decide_min_max(map, key, value, min, max) when is_nil(min) and is_nil(max) do
-    StreamData.map_of(key, value)
   end
 end
